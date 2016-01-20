909bc9eddb1223c77fc809ab6bb88dc01fb59b79
Jota Vares <joe@manulith.com>
2016-01-20 12:05:59 -0500

    adds fixdata route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,6 +15,17 @@ Route::get('/', ['as' => 'home', 'uses' => function () {
     return view('welcome');
 }]);
 
+Route::get('/fixdata', function(){
+	foreach(\App\Poll::where('state_id', 1)->get() as $poll){
+		$poll->state_id = 15;
+		$poll->save();
+	}
+	foreach(\App\Turnout::where('state_id', 1)->get() as $turnout){
+		$turnout->state_id = 15;
+		$turnout->save();
+	}
+})
+
 Route::resource('turnouts', 'TurnoutsController');
 Route::resource('polls', 'PollsController');
 Route::post('polls/updatepoll', ['as' => 'updatepoll', 'uses' => 'PollsController@updatepoll']);
