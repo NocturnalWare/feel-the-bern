0313510677b4fa611964c1d2203815a5fb4f4718
Jota Vares <joe@manulith.com>
2016-01-20 12:13:14 -0500

    updates dropdown

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -45,11 +45,43 @@
         </style>
     </head>
     <body>
-    <select class="form-control col-sm-12" id="showState" value="15">
-        @foreach(\App\StateList::all() as $state)
-            <option value="{{$state->id}}" @if($state->id == 15) selected @endif >{{$state->state_name}}</option>
-        @endforeach
-    </select>
+
+
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">Bernie Poll Analysis</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav navbar-right">
+            <li>
+            <h1>Currently Viewing  </h1>
+            </li>
+            <li>
+            <select class="form-control" style="height:300%;font-size:3em" id="showState" value="15">
+                @foreach(\App\StateList::all() as $state)
+                    <option value="{{$state->id}}" @if($state->id == 15) selected @endif >{{$state->state_name}}</option>
+                @endforeach
+            </select>
+            </li>
+
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+
+
+
+
 
     <script type="text/javascript">
         $('#showState').on('change', function(){
